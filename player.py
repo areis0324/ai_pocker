d8f3777ecd6880301f5f2217ff3ac48c9f9426ea
Austin Kau <austin_kau@trend.com.tw>
2018-06-22 18:51:02 +0800

    add BasicPlayer for baseline

--- player.py
+++ player.py
@@ -2,8 +2,6 @@ from talker.client import PokerClient
 from talker.action import *
 
 
-
-
 class RNNPlayer(PokerClient):
     CLIENT_NAME = "austin"
 
@@ -11,6 +9,21 @@ class RNNPlayer(PokerClient):
         return RAISE
 
 
+class BasicPlayer(PokerClient):
+    CLIENT_NAME = "austin"
+
+    def predict(self):
+        if self.my_card_raking == -1:
+            return CALL
+        elif self.my_card_raking > 5000:
+            return FOLD
+        elif self.my_card_raking > 3000:
+            return CALL
+        return RAISE
+
+
+
+
 if __name__ == '__main__':
-    pc = RNNPlayer()
+    pc = BasicPlayer()
     pc.doListen()
